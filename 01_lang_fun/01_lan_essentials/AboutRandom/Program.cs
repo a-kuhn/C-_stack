28c42fa4e759314cebf8dc9d2a7c743e530e1390
alex k <alex.kuhn1123@gmail.com>
2020-04-05 14:50:19 -0700

    using Random

---
+++ 01_lang_fun/01_lan_essentials/AboutRandom/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AboutRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // namespace System has Random built in. to use, first need to create a random object
+            Random rand = new Random();
+            // then call .Next method on random object (rand)
+            for (int val = 0; val < 10; val++)
+            {
+                //.Next takes two optional arguments lowest (inclusive) and highest (exclusive)
+                Console.WriteLine("here's a random number between -2 and 18: " + rand.Next(-2, 18));
+            }
+        }
+    }
+}
