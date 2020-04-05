162b901ae95764f2d15fd37195bec7dad216217a
alex k <alex.kuhn1123@gmail.com>
2020-04-05 14:37:46 -0700

    Loops: practice with for and while loops

--- 01_lang_fun/01_lan_essentials/Loops/Program.cs
+++ 01_lang_fun/01_lan_essentials/Loops/Program.cs
@@ -6,7 +6,30 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("for loop including 5 (<=)");
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("for loop excluding 5 (<)");
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("for loops using variables");
+            int start = 0;
+            int end = 5;
+            for (int i = start; i <= end; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("while loops ");
+            int j = 0;
+            while (j < 6)
+            {
+                Console.WriteLine(j);
+                j = j + 1;
+            }
         }
     }
 }
