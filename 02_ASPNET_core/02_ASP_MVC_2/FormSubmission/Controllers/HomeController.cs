c35ccad9a8f3b752b3ed115b124ba577c3109cea
alex k <alex.kuhn1123@gmail.com>
2020-04-13 14:20:54 -0700

    completed FormSubmission

---
+++ 02_ASPNET_core/02_ASP_MVC_2/FormSubmission/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FormSubmission.Models;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("result")]
+        public IActionResult Result(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                // redirect to success page
+                return RedirectToAction("Success");
+            }
+            else
+            {
+                // to display errors
+                return View("Index");
+            }
+        }
+
+        [HttpGet("success")]
+        public string Success()
+        {
+            return "Successfully Submitted!";
+        }
+    }
+}
---
+++ 02_ASPNET_core/02_ASP_MVC_2/FormSubmission/Models/User.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+namespace FormSubmission.Models
+{
+    public class User
+    {
+        [Required]
+        [MinLength(4)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Range(0, 1000)]
+        public int Age { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+
+        public User() { }
+        public User(User newUser)
+        {
+            FirstName = newUser.FirstName;
+            LastName = newUser.LastName;
+            Age = newUser.Age;
+            Email = newUser.Email;
+            Password = newUser.Password;
+        }
+    }
+}
