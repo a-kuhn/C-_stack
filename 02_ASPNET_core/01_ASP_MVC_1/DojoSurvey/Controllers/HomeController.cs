5d4f2db9c85d3b151803f09eeed01ba918b0aab5
alex k <alex.kuhn1123@gmail.com>
2020-04-12 17:26:48 -0700

    completed DojoSurvey

--- 02_ASPNET_core/01_ASP_MVC_1/DojoSurvey/Controllers/HomeController.cs
+++ 02_ASPNET_core/01_ASP_MVC_1/DojoSurvey/Controllers/HomeController.cs
@@ -12,26 +12,16 @@ namespace DojoSurvey.Controllers
             return View();
         }
 
-        [HttpPost("formSubmit")]
-        public IActionResult FormSubmit(string Name, string DojoLocation, string FavLang, string Comment)
-        {
-            string inputName = Name;
-            string inputDojoLocation = DojoLocation;
-            string inputFavLang = FavLang;
-            string inputComment = Comment;
-
-            return RedirectToAction("Result", new { Name = inputName, DojoLocation = inputDojoLocation, FavLang = inputFavLang, Comment = inputComment });
-        }
-
-        //handle form submit
-        [HttpGet("result")]
+        [HttpPost("result")]
         public IActionResult Result(string Name, string DojoLocation, string FavLang, string Comment)
         {
             ViewBag.Name = Name;
             ViewBag.DojoLocation = DojoLocation;
             ViewBag.FavLang = FavLang;
             ViewBag.Comment = Comment;
+
             return View();
         }
+
     }
 }
