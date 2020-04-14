64e239e84be0627cecde6efae56b82ad6558f9e7
alex k <alex.kuhn1123@gmail.com>
2020-04-14 14:14:40 -0700

    random generator working

--- 02_ASPNET_core/02_ASP_MVC_2/RandomPasscode/Controllers/HomeController.cs
+++ 02_ASPNET_core/02_ASP_MVC_2/RandomPasscode/Controllers/HomeController.cs
@@ -5,25 +5,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RandomPasscode.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace RandomPasscode.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet("")]
         public IActionResult Index()
         {
+            //create count for # of passcode generations
+            int count = 0;
+            HttpContext.Session.SetInt32("Count", count + 1);
+            int? codeCount = HttpContext.Session.GetInt32("Count");
+
+            //generate a 14-char random passcode
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random rand = new Random();
+            string randPass = "";
+            for (int i = 0; i < 14; i++)
+            {
+                randPass += chars[rand.Next(0, chars.Length)];
+            }
+
+            //add random passcode to session
+            HttpContext.Session.SetString("RandomPasscode", randPass);
+            string RandomPasscode = HttpContext.Session.GetString("RandomPasscode");
+
+            //send session data to View
+            ViewBag.Count = codeCount;
+            ViewBag.RandomPasscode = randPass;
+
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- 02_ASPNET_core/02_ASP_MVC_2/RandomPasscode/Startup.cs
+++ 02_ASPNET_core/02_ASP_MVC_2/RandomPasscode/Startup.cs
@@ -23,12 +23,7 @@ namespace RandomPasscode
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            services.AddSession();
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
@@ -47,7 +42,7 @@ namespace RandomPasscode
             }
 
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+            app.UseSession();
 
             app.UseMvc(routes =>
             {
