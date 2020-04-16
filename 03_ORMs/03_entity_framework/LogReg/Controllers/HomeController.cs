cd8b61e38b802d1e186331e53ffe33f5797b5b39
alex k <alex.kuhn1123@gmail.com>
2020-04-16 12:12:00 -0700

    started LogReg

---
+++ 03_ORMs/03_entity_framework/LogReg/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LogReg.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LogReg.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View("Register");
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(User newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                if (DbContext.Users.Any(u => u.Email == newUser.Email))
+                {
+                    ModelState.AddModelError("Email", "Email already in use!");
+                }
+            }
+        }
+
+    }
+}
---
+++ 03_ORMs/03_entity_framework/LogReg/Models/LogRegContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LogReg.Models
+{
+    public class LogRegContext : DbContext
+    {
+        public LogRegContext(DbContextOptions options) : base(options) { }
+    }
+}
