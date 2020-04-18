0b9c8e25844f6b64b1012551d326c5d2d40cae3c
alex k <alex.kuhn1123@gmail.com>
2020-04-18 14:42:15 -0700

    completed models and db setup

--- 03_ORMs/03_entity_framework/ChefsNDishes/Models/Chef.cs
+++ 03_ORMs/03_entity_framework/ChefsNDishes/Models/Chef.cs
@@ -1,7 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace ChefsNDishes.Models
 {
     public class Chef
     {
-        
+        [Key]
+        public int ChefId { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime DoB { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        public List<Dish> ChefsDishes { get; set; }
+
     }
 }
--- 03_ORMs/03_entity_framework/ChefsNDishes/Models/Dish.cs
+++ 03_ORMs/03_entity_framework/ChefsNDishes/Models/Dish.cs
@@ -1,7 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+
 namespace ChefsNDishes.Models
 {
     public class Dish
     {
-        
+        [Key]
+        public int DishId { get; set; }
+
+        [Required]
+        [MaxLength(45)]
+        public string Name { get; set; }
+
+        [Required]
+        public int Tastiness { get; set; }
+
+        [Required]
+        public int Calories { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [Required]
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        [Required]
+        public Chef DishCreator { get; set; }
     }
 }
